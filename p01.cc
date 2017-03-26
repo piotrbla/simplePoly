3b3b58687c2da90156b6cb73ef5b80c3e8ff3a8d
piotbla <piotb@gmail.com>
2017-03-26 19:57:09 +0200

    Clear debug info and imports

--- p01.cc
+++ p01.cc
@@ -1,12 +1,8 @@
 #include <isl/set.h>
-#include <isl/ctx.h>
-#include <isl/union_set.h>
-#include <isl/union_map.h>
 #include <isl/options.h>
 #include <pet.h>
 
 #include <iostream>
-#include <string>
 
 #include "scopinfo.hh"
 
@@ -21,13 +17,10 @@ int main(int argc, char *argv[])
     isl_ctx* ctx = isl_ctx_alloc_with_options(&pet_options_args, pet_defaults);  
     //isl_ctx* ctx = isl_ctx_alloc();
     pet_options_set_autodetect(ctx, 0);//TODO: find out why
-    logLine("Before isl");
     isl_options_set_ast_always_print_block(ctx, 1);
-    logLine("Before scop");
     const char *filename = "file.c";
     struct pet_scop* scop = 
       pet_scop_extract_from_C_source(ctx, filename, NULL);
-    logLine("After scop");
     if (NULL == scop)
     {
         std::string message = "No SCoP in file: ";
